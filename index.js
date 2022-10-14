a4dddb42e46954c10f44bb849b66c667781c6d5c
Jay P Hodges <jayphodges@gmail.com>
2022-10-14 17:48:34 -0600

    lab completed, although last function is rather weird

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective (word = "special") {
+    return function (flair = `*`) {
+        let string = word + flair + word
+        return `You are ${string}!`
+    }
+}
